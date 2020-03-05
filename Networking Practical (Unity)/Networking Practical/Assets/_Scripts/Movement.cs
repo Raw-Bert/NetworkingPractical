9ebb8ba41dede93f8e9e99507534e4a538a0f6e8
Josh <joshua.sankarlal@ontariotechu.net>
2020-03-05 10:34:09 -0500

    Basic movement

---
+++ Networking Practical (Unity)/Networking Practical/Assets/_Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int speed;
+    public int rotateSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        
+        float rotate = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+    
+        transform.Translate(0, 0, verticalMove);
+        transform.Rotate(0, rotate, 0);
+
+        
+    }
+}
